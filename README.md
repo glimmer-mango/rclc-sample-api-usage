a3849b440b175ce1a7f20f3d2259771c6ca38fbf
Sabrina Atienza <glimmer_mango@proton.me>
2023-04-24 11:33:28 -0600

    Cleaned up train script with new model example

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 ```bash
 python -m venv .venv
-source ./.venv/bin/activate
+source .venv/Scripts/activate
 pip install -r requirements.txt
 ```
 
--- train.py
+++ train.py
@@ -2,52 +2,36 @@ import os
 import requests
 
 API_KEY = os.getenv("API_KEY", "my-api-key")
-API_URL = 'https://rclc.api.com'
+API_URL = 'http://localhost:8000'
 
 def main():
+    # pass api key in header
+    headers = {'api_key': API_KEY}
+    print("Using API key", API_KEY)
+
     # upload a training dataset
-    with open('path-to-my-data.zip', 'rb') as fdata:
-        files = {'upload_file': fdata}
-        values = {'name': 'my fancy dataset name'}
-        r = requests.post(f"{API_URL}/datasets", files=files, data=values)
+    with open('./data/lymph-train-100.zip', 'rb') as fdata:
+        files = {'data': fdata}
+        params = {'name': 'lymph-train-100-final'} # change to your desired dataset name
+        print("Please wait while your dataset is being uploaded to the API...")
+        r = requests.post(f"{API_URL}/datasets", headers=headers, files=files, params=params)
         data_train = r.json()
+        print("Upload is complete. Dataset id is", data_train)
 
     # train a new model
-    r = requests.post(f"{API_URL}/models", json={
-        'dataset_id': data_train['id'],
+    print("Starting new model training session...")
+    r = requests.post(f"{API_URL}/models", headers=headers, json={
+        'dataset_id': data_train['dataset_id'],
         'strategy': 'new',
-        'name': 'string',
+        'name': '3rd time charm', # change to your desired model name
         'test_size': 0.1,
         'eval_type': 'naive-bayes',
         'channel_pick': 'combine',
         'rcl_ticks': 10,
     })
+    print("Check your dashboard to get the new model id")
 
-    # TODO: get a model from job id
-    job = r.json()
-
-    # upload a test dataset
-    with open('path-to-my-test-data.zip', 'rb') as fdata:
-        files = {'upload_file': fdata}
-        values = {'name': 'my fancy test dataset name'}
-        r = requests.post(f"{API_URL}/datasets", files=files, data=values)
-        data_test = r.json()
-
-    # test your new model
-    r = requests.post(f"{API_URL}/inferences", json={
-        'dataset_id': data_test['id'],
-        # TODO: get model_id
-        'model_id': model_id,
-    })
-
-    # TODO: download .zip results file
-    # get the results
-    r = requests.get(f"{API_URL}/results/{test_job_id}")
-    with open('path-to-zip-file', 'wb') as fd:
-        for chunk in r.iter_content(chunk_size=128):
-            fd.write(chunk)
-
-
+    # Check your dashboard to get the new model id
 
 if __name__ == '__main__':
     main()
